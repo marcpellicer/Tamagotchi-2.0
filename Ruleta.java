171109cd1dab4bf492411e0cb324f65589f136ff
Marc Pellicer Esbrí <mpelliceresbri6@gmail.com>
2025-04-29 16:18:58 +0200

    Update Ruleta.java

--- Ruleta.java
+++ Ruleta.java
@@ -1,19 +1,18 @@
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
+import java.util.Scanner;
 
-public class Ruleta extends Juego {
+public class Juego26 extends Juego {
 
-    public Ruleta(Tamagochi tamagochi) {
+    public Juego26(Tamagochi tamagochi) {
         super(tamagochi);
     }
 
     @Override
     public void jugar() {
-        Utils.escribirConEfecto("Iniciando el juego de la Ruleta...", 30);
+        Utils.escribirConEfecto("Iniciando el juego del 26...", 30);
     }
 
-    public void jugarRuleta(int apuesta, int[] numerosSeleccionados) {
+    public void jugar26(int apuesta) {
         if (apuesta > tamagochi.getDinero()) {
             Utils.escribirConEfecto("\n" + tamagochi.getNombre() + " no tiene suficiente dinero para apostar.", 30);
             return;
@@ -21,34 +20,37 @@ public class Ruleta extends Juego {
 
         tamagochi.cambiarDinero(-apuesta);
 
-        Set<Integer> bolasResultados = new HashSet<>();
-        int ganancia = 0;
-
+        int puntosJugador = 0;
+        int puntosMaquina = 0;
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
-            int bolaResultado;
-            do {
-                bolaResultado = random.nextInt(20) + 1;
-            } while (bolasResultados.contains(bolaResultado));
-            bolasResultados.add(bolaResultado);
-            Utils.escribirConEfecto("\nLa bola " + (i + 1) + " ha caído en el número: " + bolaResultado, 30);
-
-            boolean acertado = false;
-            for (int numero : numerosSeleccionados) {
-                if (bolaResultado == numero) {
-                    acertado = true;
-                    break;
-                }
+        while (true) {
+            Utils.escribirConEfecto("\nPresiona Enter para lanzar el dado: ", 30);
+            scanner.nextLine();
+            int dadoJugador = random.nextInt(6) + 1;
+            puntosJugador += dadoJugador;
+            Utils.escribirConEfecto("Jugador 1 (Tamagochi) ha sacado --> " + dadoJugador, 30);
+            if (puntosJugador > 26) {
+                puntosJugador -= dadoJugador;
             }
 
-            if (acertado) {
-                ganancia += apuesta * 2;
-            } else {
-                ganancia = 0;
+            int dadoMaquina = random.nextInt(6) + 1;
+            puntosMaquina += dadoMaquina;
+            Utils.escribirConEfecto("Jugador 2 (Máquina) ha sacado --> " + dadoMaquina, 30);
+            if (puntosMaquina > 26) {
+                puntosMaquina -= dadoMaquina;
+            }
+
+            Utils.escribirConEfecto("\nJugador 1 (Tamagochi): " + puntosJugador, 30);
+            Utils.escribirConEfecto("Jugador 2 (Máquina): " + puntosMaquina, 30);
+
+            if (puntosJugador == 26 || puntosMaquina == 26) {
+                break;
             }
         }
 
+        int ganancia = puntosJugador == 26 ? apuesta * 2 : -apuesta;
         tamagochi.cambiarDinero(ganancia);
         Utils.escribirConEfecto(ganancia > 0
                 ? "\n" + tamagochi.getNombre() + " ha ganado --> " + ganancia + " $"
