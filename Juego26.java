04f7cecb57399dbff72b3bcd4cb1369f040f37f3
Marc Pellicer Esbrí <mpelliceresbri6@gmail.com>
2025-04-30 02:46:40 +0200

    Update Juego26.java

--- Juego26.java
+++ Juego26.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Juego26 extends Juego {
 
@@ -7,6 +8,10 @@ public class Juego26 extends Juego {
     }
 
     @Override
+    public void jugar() {
+        Utils.escribirConEfecto("Iniciando el juego del 26...", 30);
+    }
+
     public void jugar26(int apuesta) {
         if (apuesta > tamagochi.getDinero()) {
             Utils.escribirConEfecto("\n" + tamagochi.getNombre() + " no tiene suficiente dinero para apostar.", 30);
@@ -14,27 +19,52 @@ public class Juego26 extends Juego {
         }
 
         tamagochi.cambiarDinero(-apuesta);
+
         int puntosJugador = 0;
         int puntosMaquina = 0;
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            Utils.escribirConEfecto("\nPresiona Enter para lanzar el dado: ", 30);
+            scanner.nextLine();
             int dadoJugador = random.nextInt(6) + 1;
             puntosJugador += dadoJugador;
-            if (puntosJugador > 26) puntosJugador -= dadoJugador;
+            Utils.escribirConEfecto("Jugador 1 (Tamagochi) ha sacado --> " + dadoJugador, 30);
+            if (puntosJugador > 26) {
+                puntosJugador -= dadoJugador;
+            }
 
             int dadoMaquina = random.nextInt(6) + 1;
             puntosMaquina += dadoMaquina;
-            if (puntosMaquina > 26) puntosMaquina -= dadoMaquina;
+            Utils.escribirConEfecto("Jugador 2 (Máquina) ha sacado --> " + dadoMaquina, 30);
+            if (puntosMaquina > 26) {
+                puntosMaquina -= dadoMaquina;
+            }
 
-            if (puntosJugador == 26 || puntosMaquina == 26) break;
+            Utils.escribirConEfecto("\nJugador 1 (Tamagochi): " + puntosJugador, 30);
+            Utils.escribirConEfecto("Jugador 2 (Máquina): " + puntosMaquina, 30);
+
+            if (puntosJugador == 26 || puntosMaquina == 26) {
+                break;
+            }
         }
 
-        int ganancia = GameRules.calcularGanancia("Juego26", puntosJugador, apuesta);
-        GameRules.aplicarCambiosEstado(tamagochi, ganancia, 10, 6);
+        
+        if (puntosJugador == puntosMaquina) {
+            tamagochi.cambiarDinero(apuesta); // Recuperar dinero inicial en caso de empate
+            Utils.escribirConEfecto("\n¡Empate! Recuperas tu apuesta inicial.", 30);
+        } else if (puntosJugador == 26) {
+            tamagochi.cambiarDinero(apuesta * 2);
+            Utils.escribirConEfecto("\n¡Has ganado! Gancia en el juego --> " + (apuesta * 2) + " $", 30);
+        } else {
+            tamagochi.cambiarDinero(-apuesta * 3);
+            Utils.escribirConEfecto("\n¡Has perdido! Pérdida en el juego --> " + (apuesta * 3) + " $", 30);
+        }
 
-        Utils.escribirConEfecto(ganancia > 0 ?
-                tamagochi.getNombre() + " ha ganado $" + ganancia :
-                tamagochi.getNombre() + " ha perdido $" + (-ganancia), 30);
+        tamagochi.cambiarSueno(10);
+        tamagochi.cambiarHambre(6);
+        tamagochi.avanzarTiempo(30);
+        tamagochi.evaluarEstado();
     }
 }
